43db678075910230e23e93ae283b67f2b7823733
Muhammad Rehman <thatsocliche@hotmail.com>
2016-10-14 14:55:41 -0400

    Added reading file text data -> matrix conversion function for kNN dating module

---
+++ 02_Classifying with k-Nearest Neighbors/kNNDating.py
@@ -0,0 +1,44 @@
+from numpy import *
+import operator
+
+def createDataSet():
+	group = array([[1.0,1.1], [1.0,1.0], [0,0], [0,0.1]])
+	labels = ['A','A','B','B']
+	return group, labels
+
+	
+def classifyKNN(testpoint, dataSet, labels, k):
+	dataSetSize = dataSet.shape[0]
+	diffMatrix = tile(testpoint,(dataSetSize,1)) - dataSet
+	squareDiffMatrix = diffMatrix ** 2
+	squareDistances = squareDiffMatrix.sum(axis=1)
+	distances = squareDistances**0.5
+	sortedDistancesIndices = distances.argsort() #Returns indices that would sort array
+
+	classCount = {}
+	for i in range(k):
+		voteILabel = labels[sortedDistancesIndices[i]]
+		classCount[voteILabel] = classCount.get(voteILabel,0) + 1
+
+	sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+	return sortedClassCount[0][0]
+
+
+def fileToMatrix(filename):
+	fr = open(filename)
+	numberOfLines = len(fr.readlines())
+	#n rows, 3 columns
+	returnMatrix = zeros((numberOfLines,3))
+	classLabelVector=[]
+
+	fr = open(filename)
+	index = 0
+	for line in fr.readlines():
+		line = line.strip()
+		listFromLine = line.split('\t')
+		#Class label is at index 3 of listFromLine
+		returnMatrix[index,:] = listFromLine[0:3]
+		#Negative indexing in Python
+		classLabelVector.append(listFromLine[-1])
+		index+=1
+	return returnMatrix,classLabelVector
