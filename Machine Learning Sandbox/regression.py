ffda48336ccda23e4bb6ce62fbb39b89615d8557
Muhammad Rehman <thatsocliche@hotmail.com>
2016-10-15 01:50:33 -0400

    Forecasting 1% into future : Training Set

--- Machine Learning Sandbox/regression.py
+++ Machine Learning Sandbox/regression.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import quandl
+import math
 
 
 df = quandl.get("WIKI/GOOGL")
@@ -10,6 +11,16 @@ df['HighLow_Volatility'] = (df['Adj. High']- df['Adj. Low'])/df['Adj. High'] * 1
 df['OpenClose_Change'] = (df['Adj. Close']- df['Adj. Open'])/df['Adj. Open'] * 100 
 
 #Redefining the df
-df = df[['Adj. Close','OpenClose_Change','HighLow_Volatility','Adj. Volume']]
-print(df.head())
-print(1)
+df = df[['Adj. Close','OpenClose_Change','HighLow_Volatility','Adj. Volume']] #Our Features 
+
+#We want to try and forecast the Adj. Close
+forecast_column = 'Adj. Close'
+df.fillna(-9999, inplace=True) #fills the NaN with the value we pass
+
+forecast_out = int(math.ceil(0.01*len(df))) #Percentage of our data to forecast out
+
+df['label'] = df[forecast_column].shift(-forecast_out) 
+#Features are what may cause our label to change 
+
+df.dropna()
+print(df.tail())
